090560a2713aa2d064cb07048cf003dc574475db
dalvinxo <dalvingamer@gmail.com>
2020-06-26 22:13:53 -0400

    first commit

---
+++ ItlaBanking/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ItlaBanking.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
